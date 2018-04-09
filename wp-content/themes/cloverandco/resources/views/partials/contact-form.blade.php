003b7ccc1096d249d72750690524706703c14760
Carly <carly.ewasiuk94@gmail.com>
2018-04-09 01:37:56 -0700

    add labels for inputs, add empty options so they default to empty

--- wp-content/themes/cloverandco/resources/views/partials/contact-form.blade.php
+++ wp-content/themes/cloverandco/resources/views/partials/contact-form.blade.php
@@ -1,21 +1,31 @@
 @php (ContactForm::validate_form()) 
 <form action=<?php the_permalink(); ?> method='POST'>
 <div class="control">
-  <input class="input" type="text" name='contact-name' placeholder="Full Name" required >
+  <label class='form-label' for='contact-name'>Your Name</label>
+  <input class="input" id='contact-name' type="text" name='contact-name' placeholder="Full Name" required autocomplete='name'>
 </div>
 <div class="control">
-  <input class="input" type="email" name='contact-email' placeholder="Email Address" required>
+<label class='form-label' for='contact-email'>Your Email</label>
+  <input class="input" id='contact-email' type="email" name='contact-email' placeholder="Email Address" required autocomplete='email'>
 </div>
 <div class="control">
-  <input class="input" type="tel" name='contact-tel' placeholder="Phone Number" required>
+<label class='form-label' for='contact-tel'>Your Phone Number</label>
+  <input class="input" type="tel" id='contact-tel' name='contact-tel' placeholder="Phone Number" required autocomplete='tel'>
 </div>
 <div class="control">
-  <input class="input" type="date" name='event-date' placeholder="Date Of Event">
+<label class='form-label' for='guest-estimate'>Estimated Number of Guests</label>
+  <input class="input" type="number" id='guest-estimate' name='guest-estimate' placeholder="Estimated Number of Guests">
+</div>
+<div class="control">
+<label class='form-label' for='event-date'>Your Event Date</label>
+  <input class="input" type="text" id='event-date' name='event-date' placeholder="Date Of Event">
 </div>
 
 <div class="control is-expanded">
+<label class='form-label' for='event-type'>Your Event Type</label>
   <div class="select is-fullwidth">
-    <select name='event-type'>
+    <select name='event-type' id='event-type'>
+      <option label=" "></option>
       <option>Birthday Party</option>
       <option>Wedding</option>
       <option>Engagement</option>
@@ -33,18 +43,18 @@
       <option>Graduation</option>
       <option>New Years</option>
       <option>Other</option>
-      <option selected>Type Of Event</option>
     </select>
   </div>
 </div>
 
 <div class="control is-expanded">
+<label class='form-label' for='event-service'> Your Service </label>
   <div class="select is-fullwidth">
     <select name='service'>
+      <option label=" "></option>
       <option>Candy Buffet</option>
       <option>Donut Wall</option>
       <option>Goody Bags</option>
-      <option selected>Service Of Interest</option>
     </select>
   </div>
 </div>
