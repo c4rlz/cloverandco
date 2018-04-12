5b7d02f930379c539a910d6413ed2d6970b38b7f
Carly <carly.ewasiuk94@gmail.com>
2018-04-12 00:53:26 -0700

    content: copyright and social links

--- wp-content/themes/cloverandco/resources/views/partials/footer.blade.php
+++ wp-content/themes/cloverandco/resources/views/partials/footer.blade.php
@@ -1,5 +1,12 @@
 <footer class="content-info">
   <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
+    <!-- @php(dynamic_sidebar('sidebar-footer')) -->
+    <span>© 2018 Clover & Co Candy Catering 
+    </span>
+    <section class='social-icons'>
+      <a href='https://instagram.com/cloverscandycatering'><i class="fa fa-instagram" aria-hidden="true"></i></a>
+      <a href='https://www.facebook.com/cloverscandycatering/'><i class="fa fa-facebook" aria-hidden="true"></i></a>
+      <a href='https://twitter.com/cloverscandycatering'><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </section>
   </div>
 </footer>
